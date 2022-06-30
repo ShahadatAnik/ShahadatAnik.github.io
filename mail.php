da8464c5d54d589641353e968e5fe62c5f0b9d68
Shahadat Anik <git.shahadat.anik@gmail.com>
2022-06-30 22:18:36 +0600

    updated about info for UTA

--- mail.php
+++ mail.php
@@ -1,23 +1,36 @@
 <?php
 //get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$subjectUser= $_POST['subject'];
-$message= $_POST['message'];
+$server = "sql202.epizy.com";
+$username = "epiz_31992531";
+$password = "rvBEP9MNeC";
+$dbname = "epiz_31992531_portfoliowebsite";
 
-$to = "shahadatanik777@gmail.com";
+$conn = mysqli_connect($server, $username, $password, $dbname);
 
-$subject = "Mail From Shahadat Anik website";
-
-$txt ="Name: ". $name . "\r\n  Email: " . $email . "\r\n  Subject: " . $subjectUser . "\r\n Message: " . $message;
-
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+}
+else{
+    $name = $_POST['name'];
+    $email= $_POST['email'];
+    $subjectUser= $_POST['subject'];
+    $message= $_POST['message'];
+    
+    $to = "shahadatanik777@gmail.com";
+    
+    $subject = "Mail From Shahadat Anik website";
+    
+    $txt ="Name: ". $name . "\r\n  Email: " . $email . "\r\n  Subject: " . $subjectUser . "\r\n Message: " . $message;
+    
+    $headers = "From: noreply@yoursite.com" . "\r\n" .
+    "CC: somebodyelse@example.com";
+    
+    if($email!=NULL){
+        mail($to,$subject,$txt,$headers);
+    }
+    
+    //redirect
+    header("Location:contact.html");
 }
 
-//redirect
-header("Location:contact.html");
 ?>
